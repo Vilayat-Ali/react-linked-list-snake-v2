0b90c7bf265a4c0606953631071b0080c9f2b711
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2022-12-28 12:29:15 +0530

    Added tech card

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,83 @@
-type Props = {};
+// lib
+import { ReactNode } from "react";
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+  Code,
+} from "@chakra-ui/react";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import moment from "moment";
 
-const index = (props: Props) => {
-  return <div>index</div>;
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
 };
 
-export default index;
+const Footer = () => {
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Text>
+          © {moment().format("YYYY")} <Code>Syed Vilayat Ali Rizvi</Code>. All
+          rights reserved
+        </Text>
+        <Stack direction={"row"} spacing={6}>
+          <SocialButton label={"Twitter"} href={"#"}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={"YouTube"} href={"#"}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={"Instagram"} href={"#"}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/components/cards/TechCard/index.tsx
+++ src/components/cards/TechCard/index.tsx
@@ -1,7 +1,116 @@
-type Props = {};
+// lib
+import { useState, MouseEvent } from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  Img,
+  Flex,
+  Center,
+  useColorModeValue,
+  HStack,
+} from "@chakra-ui/react";
+import { BsArrowUpRight, BsHeartFill, BsHeart } from "react-icons/bs";
 
-const TechCard = (props: Props) => {
-  return <div>TechCard</div>;
+type PropType = {
+  tag: string;
+  title: string;
+  description: string;
+};
+
+const TechCard = ({ tag, title, description }: PropType) => {
+  // states
+  const [liked, setLiked] = useState(false);
+
+  // handle like
+  const handleLike = (e: MouseEvent<Element>) => {
+    setLiked(!liked);
+  };
+
+  // handle view more
+  const handleViewMore = (url: string) => {
+    window.location.href = url;
+  };
+
+  return (
+    <Center py={6}>
+      <Box
+        w="xs"
+        rounded={"sm"}
+        my={5}
+        mx={[0, 5]}
+        overflow={"hidden"}
+        bg="white"
+        border={"1px"}
+        borderColor="black"
+        boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 cyan")}
+      >
+        <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+          <Img
+            src={
+              "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+            }
+            roundedTop={"sm"}
+            objectFit="cover"
+            h="full"
+            w="full"
+            alt={"Blog Image"}
+          />
+        </Box>
+        <Box p={4}>
+          <Box
+            bg="black"
+            display={"inline-block"}
+            px={2}
+            py={1}
+            color="white"
+            mb={2}
+          >
+            <Text fontSize={"xs"} fontWeight="medium">
+              {tag}
+            </Text>
+          </Box>
+          <Heading color={"black"} fontSize={"2xl"} noOfLines={1}>
+            {title}
+          </Heading>
+          <Text color={"gray.500"} noOfLines={3}>
+            {description}
+          </Text>
+        </Box>
+        <HStack borderTop={"1px"} color="black">
+          <Flex
+            p={4}
+            alignItems="center"
+            justifyContent={"space-between"}
+            roundedBottom={"sm"}
+            cursor={"pointer"}
+            w="full"
+            onClick={() => handleViewMore("")}
+          >
+            <Text fontSize={"md"} fontWeight={"semibold"}>
+              View more
+            </Text>
+            <BsArrowUpRight />
+          </Flex>
+          <Flex
+            p={4}
+            alignItems="center"
+            justifyContent={"space-between"}
+            roundedBottom={"sm"}
+            borderLeft={"1px"}
+            cursor="pointer"
+            onClick={(e: MouseEvent<Element>) => handleLike(e)}
+          >
+            {liked ? (
+              <BsHeartFill fill="red" fontSize={"24px"} />
+            ) : (
+              <BsHeart fontSize={"24px"} />
+            )}
+          </Flex>
+        </HStack>
+      </Box>
+    </Center>
+  );
 };
 
 export default TechCard;
